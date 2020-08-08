def2f1ed0af6c0a0521f02b775d5ecd76ee91e95
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-08-08 11:09:31 -0400

    refactor: move styles to a different module

--- app/screens/question-screen/question-screen.tsx
+++ app/screens/question-screen/question-screen.tsx
@@ -1,21 +1,11 @@
 import React, { FunctionComponent as Component } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, View } from "react-native"
+import { View } from "react-native"
 import { Screen, Text } from "../../components"
+import * as styles from './styles'
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../models"
-import { color, spacing } from "../../theme"
 
-const ROOT: ViewStyle = {
-  flex: 1,
-  paddingHorizontal: spacing.large,
-  backgroundColor: color.palette.black,
-}
-
-const HEADER_CONTAINER: ViewStyle = {
-  marginTop: spacing.large,
-  marginBottom: spacing.medium,
-}
 
 export const QuestionScreen: Component = observer(function QuestionScreen() {
   // const rootStore = useStores()
@@ -23,8 +13,8 @@ export const QuestionScreen: Component = observer(function QuestionScreen() {
   // Pull in navigation via hook
   // const navigation = useNavigation()
   return (
-    <Screen style={ROOT} preset="scroll">
-      <View style={HEADER_CONTAINER}>
+    <Screen style={styles.ROOT} preset="scroll">
+      <View style={styles.HEADER_CONTAINER}>
         <Text preset="header" tx="questionScreen.header" />
       </View>
     </Screen>
---
+++ app/screens/question-screen/styles.ts
@@ -0,0 +1,13 @@
+import { ViewStyle } from "react-native"
+import { color, spacing } from "../../theme"
+
+export const ROOT: ViewStyle = {
+  flex: 1,
+  paddingHorizontal: spacing.large,
+  backgroundColor: color.palette.black,
+}
+
+export const HEADER_CONTAINER: ViewStyle = {
+  marginTop: spacing.large,
+  marginBottom: spacing.medium,
+}
